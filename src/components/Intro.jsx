6b5ec4f1954f011a4d825495a9070d996e90e059
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-05-28 19:58:51 +0700

    Add images

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,13 @@
 import profileImage from '../assets/images/profilepic.png'
+import htmlImage from '../assets/images/html.png'
+import cssImage from '../assets/images/css.png'
+import jsImage from '../assets/images/js.png'
+import reactImage from '../assets/images/react.png'
+import viteImage from '../assets/images/vite.png'
+import bootstrapImage from '../assets/images/bootstrap.png'
+import sassImage from '../assets/images/sass.png'
+import tailwindImage from '../assets/images/tailwind.png'
+import { RxLinkedinLogo, RxGithubLogo } from 'react-icons/rx'
 
 const Intro = () => {
   return (
@@ -7,17 +16,38 @@ const Intro = () => {
         <div id="intro-main" className="flex ">
           <div id="intro-main-text" className="w-7/12 ">
             <h1 className="pb-2 text-5xl font-bold font-poppins">Hi! I'm M. Arifien Syachrizal</h1>
-            <h2 className="pb-2 text-3xl font-bold font-oxygen-mono">Front-End Web Developer</h2>
-            <p className="font-poppins">I create visually stunning and intuitive web experiences with a focus on seamless interactions and captivating design. Let's embark on a journey of exceptional web development together!</p>
+            <h2 className="pb-2 text-3xl font-bold font-oxygen-mono text-red-700">Front-End Web Developer</h2>
+            <p className="font-poppins leading-7 tracking-wide">I create visually stunning and intuitive web experiences with a focus on seamless interactions and captivating design. Let's embark on a journey of exceptional web development together!</p>
+            <div className="flex gap-3">
+              <a href="https://www.linkedin.com/in/arifiensr/" target="_blank">
+                <RxLinkedinLogo className="mt-8 text-4xl hover:text-red-700 transition-all duration-[400ms]" />
+              </a>
+              <a href="https://github.com/arifiensr" target="_blank">
+                <RxGithubLogo className="mt-8 text-4xl hover:text-red-700 transition-all duration-[400ms]" />
+              </a>
+            </div>
           </div>
-          <div id="intro-main-image" className="w-5/12 overflow-auto rounded-full">
-            <img src={profileImage} className="object-cover w-full h-full " />
+          <div id="intro-main-image" className="flex items-center justify-center w-5/12">
+            <div className="w-2/3 h-auto overflow-hidden rounded-full">
+              <img src={profileImage} className="object-contain w-full h-full " />
+            </div>
           </div>
         </div>
 
-        <div id="intro-skills" className="flex border-2">
-          <div className="w-3/12 border-3 border-r-black">Tech Stack</div>
-          <div className="w-9/12">Skill</div>
+        <div id="intro-skills" className="flex h-16 mt-12">
+          <div className="flex items-center justify-start w-40 h-full border-r-4 border-r-black">
+            <span className="text-2xl font-bold">Tech Stack</span>
+          </div>
+          <div className="flex flex-auto items-center justify-evenly">
+            <img src={htmlImage} className="w-auto h-full" />
+            <img src={cssImage} className="w-auto h-full" />
+            <img src={jsImage} className="w-auto h-full" />
+            <img src={reactImage} className="w-auto h-full" />
+            <img src={viteImage} className="w-auto h-full" />
+            <img src={bootstrapImage} className="w-auto h-full" />
+            <img src={sassImage} className="w-auto h-full" />
+            <img src={tailwindImage} className="w-auto h-full" />
+          </div>
         </div>
       </div>
     </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,25 @@
 const Navbar = () => {
   return (
     <section id="navbar" className="fixed top-0 flex items-center justify-between w-full h-16 p-8 bg-white w navbar drop-shadow">
-      <div id="navbar-title" className="flex-initial">
-        <span className="text-xl font-bold font-major-mono-display"><span className="text-red-500">A</span>rifiensr</span>
+      <div id="navbar-title" className="flex-initial cursor-pointer">
+        <span className="text-xl font-bold font-major-mono-display hover:text-red-700 transition-all dsf duration-[400ms]">
+          <span className="">A</span>rifiensr
+        </span>
       </div>
       <div id="navbar-links" className="flex-initial">
         <ul className="flex gap-4 font-oxygen-mono">
-          <li className="p-1 m-4 transition-all border-b-2 border-white hover:text-red-500 hover:border-red-500"><a href="#">Home</a></li>
-          <li className="p-1 m-4 transition-all border-b-2 border-white hover:text-red-500 hover:border-red-500"><a href="#about">About</a></li>
-          <li className="p-1 m-4 transition-all border-b-2 border-white hover:text-red-500 hover:border-red-500"><a href="#projects">Projects</a></li>
-          <li className="p-1 m-4 transition-all border-b-2 border-white hover:text-red-500 hover:border-red-500"><a href="#contact">Contact</a></li>
+          <li className="p-1 m-4 transition-all border-b-2 border-white hover:text-red-700 hover:border-red-700 duration-[400ms]">
+            <a href="#">Home</a>
+          </li>
+          <li className="p-1 m-4 transition-all border-b-2 border-white hover:text-red-700 hover:border-red-700 duration-[400ms]">
+            <a href="#about">About</a>
+          </li>
+          <li className="p-1 m-4 transition-all border-b-2 border-white hover:text-red-700 hover:border-red-700 duration-[400ms]">
+            <a href="#projects">Projects</a>
+          </li>
+          <li className="p-1 m-4 transition-all border-b-2 border-white hover:text-red-700 hover:border-red-700 duration-[400ms]">
+            <a href="#contact">Contact</a>
+          </li>
         </ul>
       </div>
     </section>

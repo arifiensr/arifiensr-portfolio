515351ba0eb4cd3f128bcef82d3e54f6b1de9fbc
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-05-30 16:49:22 +0700

    Fix intro heights

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -11,11 +11,11 @@ import { RxLinkedinLogo, RxGithubLogo } from 'react-icons/rx'
 
 const Intro = () => {
   return (
-    <section id="intro" className="flex items-center justify-center h-screen bg-slate-50">
-      <div className="container m-auto w-full md:w-[1000px]">
+    <section id="intro" className="flex items-center justify-center min-h-screen bg-slate-50">
+      <div className="container m-auto w-full h-full md:w-[1000px]">
         <div id="intro-main" className="flex flex-col-reverse items-center justify-center md:flex-row">
           <div id="intro-main-text" className="w-full mt-8 md:w-7/12 md:mt-0">
-            <h1 className="pb-2 text-4xl font-bold text-center md:text-5xl font-poppins md:text-start">Hi! I'm M. Arifien Syachrizal</h1>
+            <h1 className="pb-2 text-4xl font-bold text-center md:text-5xl font-poppins md:text-start">Hi! I'm Arifien</h1>
             <h2 className="pb-2 text-2xl font-bold text-center text-red-700 md:text-3xl font-oxygen-mono md:text-start">Front-End Web Developer</h2>
             <p className="leading-7 tracking-wide text-center font-poppins md:text-start">I create visually stunning and intuitive web experiences with a focus on seamless interactions and captivating design. Let's embark on a journey of exceptional web development together!</p>
             <div className="flex justify-center gap-3 md:justify-start">
@@ -35,10 +35,10 @@ const Intro = () => {
         </div>
 
         <div id="intro-skills" className="flex flex-col items-center justify-center h-auto mt-6 md:mt-12 md:h-12 md:flex-row">
-          <div className="flex items-center justify-center w-40 h-full p-3 border-b-2 md:p-0 md:w-40 md:justify-start md:border-b-0 md:border-r-2 md:border-r-black border-b-slate-500">
-            <span className="text-2xl font-bold text-center">Tech Stack</span>
+          <div className="flex items-center justify-center w-48 h-full p-3 md:p-0 md:w-52 md:justify-start ">
+            <span className="px-2 pb-4 text-xl font-bold text-center border-b-2 md:pe-8 md:pb-0 font-oxygen-mono md:border-b-0 md:border-r-2 md:border-r-slate-500 border-b-slate-500">Tech Stack</span>
           </div>
-          <div className="flex items-center flex-auto w-full mt-6 justify-evenly md:mt-0">
+          <div className="flex flex-wrap items-center justify-center w-full mt-4 gap-x-8 gap-y-4 md:justify-evenly md:mt-0 md:flex-nowrap">
             <img src={htmlImage} className="w-auto h-12" />
             <img src={cssImage} className="w-auto h-12" />
             <img src={jsImage} className="w-auto h-12" />

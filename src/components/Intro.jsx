07b9c4a029648e9fa9d2ddecf56c2b25d91ba6ce
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-05-31 11:33:05 +0700

    Add projects

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,6 +9,42 @@ import sassImage from '../assets/images/sass.png'
 import tailwindImage from '../assets/images/tailwind.png'
 import { RxLinkedinLogo, RxGithubLogo } from 'react-icons/rx'
 import { Element } from 'react-scroll'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip'
+
+const skills = [
+  {
+    title: 'HTML',
+    image: htmlImage,
+  },
+  {
+    title: 'CSS',
+    image: cssImage,
+  },
+  {
+    title: 'Javascript',
+    image: jsImage,
+  },
+  {
+    title: 'React',
+    image: reactImage,
+  },
+  {
+    title: 'Vite',
+    image: viteImage,
+  },
+  {
+    title: 'Bootstrap',
+    image: bootstrapImage,
+  },
+  {
+    title: 'SASS',
+    image: sassImage,
+  },
+  {
+    title: 'Tailwind',
+    image: tailwindImage,
+  },
+]
 
 const Intro = () => {
   return (
@@ -19,13 +55,31 @@ const Intro = () => {
             <h1 className="pb-2 text-4xl font-bold text-center md:text-5xl font-poppins md:text-start">Hi! I'm Arifien</h1>
             <h2 className="pb-2 text-2xl font-bold text-center text-red-700 md:text-3xl font-oxygen-mono md:text-start">Front-End Web Developer</h2>
             <p className="leading-7 tracking-wide text-center font-poppins md:text-start">I create visually stunning and intuitive web experiences with a focus on seamless interactions and captivating design. Let's embark on a journey of exceptional web development together!</p>
-            <div className="flex justify-center gap-3 md:justify-start">
-              <a href="https://www.linkedin.com/in/arifiensr/" target="_blank">
-                <RxLinkedinLogo className="mt-6 md:mt-8 text-4xl hover:text-red-700 transition-all duration-[400ms]" />
-              </a>
-              <a href="https://github.com/arifiensr" target="_blank">
-                <RxGithubLogo className="mt-6 md:mt-8 text-4xl hover:text-red-700 transition-all duration-[400ms]" />
-              </a>
+            <div className="flex items-center justify-center gap-3 mt-6 md:justify-start md:mt-12 ">
+              <TooltipProvider delayDuration={0}>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <a href="https://www.linkedin.com/in/arifiensr/" target="_blank">
+                      <RxLinkedinLogo className=" text-4xl hover:text-red-700 transition-all duration-[400ms]" />
+                    </a>
+                  </TooltipTrigger>
+                  <TooltipContent className="font-oxygen-mono" sideOffset={10}>
+                    <p>My LinkedIn</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <TooltipProvider delayDuration={0}>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <a href="https://github.com/arifiensr" target="_blank">
+                      <RxGithubLogo className=" text-4xl hover:text-red-700 transition-all duration-[400ms]" />
+                    </a>
+                  </TooltipTrigger>
+                  <TooltipContent className="font-oxygen-mono" sideOffset={10}>
+                    <p>My Github</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </div>
           </div>
           <div id="intro-main-image" className="flex items-center justify-center w-full mt-24 md:mt-0 md:w-5/12">
@@ -40,14 +94,20 @@ const Intro = () => {
             <span className="px-2 pb-4 text-xl font-bold text-center border-b-2 md:pe-8 md:pb-0 font-oxygen-mono md:border-b-0 md:border-r-2 md:border-r-slate-500 border-b-slate-500">Tech Stack</span>
           </div>
           <div className="flex flex-wrap items-center justify-center w-full mt-4 gap-x-8 gap-y-4 md:justify-evenly md:mt-0 md:flex-nowrap">
-            <img src={htmlImage} className="w-auto h-12" />
-            <img src={cssImage} className="w-auto h-12" />
-            <img src={jsImage} className="w-auto h-12" />
-            <img src={reactImage} className="w-auto h-12" />
-            <img src={viteImage} className="w-auto h-12" />
-            <img src={bootstrapImage} className="w-auto h-12" />
-            <img src={sassImage} className="w-auto h-12" />
-            <img src={tailwindImage} className="w-auto h-12" />
+            {skills.map((item, index) => {
+              return (
+                <TooltipProvider key={item.title + index} delayDuration={0}>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <img src={item.image} className="w-auto h-12 transition-all hover:text-red-700 duration-[400ms]" />
+                    </TooltipTrigger>
+                    <TooltipContent className="font-oxygen-mono" sideOffset={10}>
+                      <p>{item.title}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              )
+            })}
           </div>
         </div>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,83 @@
 import { Element } from 'react-scroll'
+import { Button } from './ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
+import photologueMockup from '../assets/images/photologueMockup.png'
+import { ExternalLinkIcon, GithubIcon } from 'lucide-react'
+import { FaBootstrap, FaReact, FaSass } from 'react-icons/fa'
+
+const projects = [
+  {
+    title: 'Photologue',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ullam perspiciatis placeat, libero praesentium asperiores tempore quidem magni dolore ipsum. Facere eos nam ea est obcaecati, recusandae perspiciatis porro possimus!',
+    preview: photologueMockup,
+    tech(classStyle) {
+      return (
+        <>
+          <FaReact />
+          <FaBootstrap />
+          <FaSass />
+        </>
+      )
+    },
+    github: 'https://github.com/arifiensr/photologue',
+    demo: 'https://photologue.netlify.app/',
+  },
+  {
+    title: 'Photologue',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ullam perspiciatis placeat, libero praesentium asperiores tempore quidem magni dolore ipsum. Facere eos nam ea est obcaecati, recusandae perspiciatis porro possimus!',
+    preview: photologueMockup,
+    tech() {
+      return (
+        <>
+          <FaReact />
+          <FaBootstrap />
+          <FaSass />
+        </>
+      )
+    },
+    github: 'https://github.com/arifiensr/photologue',
+    demo: 'https://photologue.netlify.app/',
+  },
+]
 
 const Projects = () => {
   return (
-    <Element name="projects" id="projects" className="flex items-center justify-center w-full min-h-screen m-auto bg-slate-50">
-      <div className="container flex items-center justify-center w-full md:w-[1000px]"></div>
+    <Element name="projects" id="projects" className="flex items-center justify-center w-full min-h-screen pt-20 m-auto bg-slate-50">
+      <div className="container flex items-center justify-center w-full md:w-[1000px] flex-col gap-4">
+        <div id="projects-title" className="flex items-center justify-center w-full">
+          <h2 className="text-2xl font-bold font-poppins">Projects</h2>
+        </div>
+        <div className="flex flex-col gap-8 card-content-wrapper">
+          {projects.map((item, index) => {
+            return (
+              <Card key={item.title + index} className="w-full font-poppins">
+                <CardContent className={`flex p-4 gap-4 flex-col ${index % 2 === 1 ? 'md:flex-row-reverse' : 'md:flex-row'}`}>
+                  <div className="w-full md:w-[500px] card-image h-auto m-auto">
+                    <img src={item.preview} className="object-cover w-full h-full rounded-lg" />
+                  </div>
+                  <div className="flex flex-col flex-1 text-center card-desc">
+                    <h1 className="pb-2 text-xl font-bold font-poppins">{item.title}</h1>
+                    <p className="pb-4">{item.description}</p>
+                    <div className="flex items-center justify-center gap-6 pb-5 text-3xl card-desc-tech">{item.tech()}</div>
+                    <div className="flex items-end justify-center w-full h-full gap-6 card-desc-button">
+                      <a href={item.github} target="_blank">
+                        <Button className="flex gap-2 hover:bg-red-700">
+                          Github <GithubIcon size={20} />
+                        </Button>
+                      </a>
+                      <a href={item.demo} target="_blank">
+                        <Button className="flex gap-2 hover:bg-red-700">
+                          Demo <ExternalLinkIcon size={20} />
+                        </Button>
+                      </a>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+            )
+          })}
+        </div>
+      </div>
     </Element>
   )
 }
